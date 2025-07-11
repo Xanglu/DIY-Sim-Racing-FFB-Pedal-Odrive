375a81ef057720962065c75fda9856c11cd9f477
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2025-07-11 19:53:17 +0800

    Update OTA_Pull.h

--- ESP32/include/OTA_Pull.h
+++ ESP32/include/OTA_Pull.h
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <ESP32OTAPull.h>
 #include <esp_wifi.h>
-#define JSON_URL_dev   "https://raw.githubusercontent.com/gilphilbert/pedal-flasher/main/json/dev/Version_ControlBoard.json"
+#define JSON_URL_dev   "https://raw.githubusercontent.com/ChrGri/DIY-Sim-Racing-FFB-Pedal/develop/OTA/DailyBuild/json/Version_ControlBoard.json"
 //#define JSON_URL_dev "https://raw.githubusercontent.com/tcfshcrw/playground/main/OTA_test_repo/GH2/Version.json"
-#define JSON_URL_main   "https://raw.githubusercontent.com/gilphilbert/pedal-flasher/main/json/main/Version_ControlBoard.json"
+#define JSON_URL_main   "https://raw.githubusercontent.com/gilphilbert/pedal-flasher/main/json/dev/Version_ControlBoard.json"
 #define JSON_URL_daily "https://raw.githubusercontent.com/ChrGri/DIY-Sim-Racing-FFB-Pedal/develop/OTA/DailyBuild/json/Version_ControlBoard.json"
 bool OTA_enable_b =false;
 bool OTA_status =false;
