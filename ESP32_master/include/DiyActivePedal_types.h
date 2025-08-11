4d86c0a12cd040bcd9fe5c0f35d49d6eb030d072
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2025-08-11 09:37:39 -0600

    make bridge compatible with DAP 153

--- ESP32_master/include/DiyActivePedal_types.h
+++ ESP32_master/include/DiyActivePedal_types.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 // define the payload revision
-#define DAP_VERSION_CONFIG 152
+#define DAP_VERSION_CONFIG 153
 
 // define the payload types
 #define DAP_PAYLOAD_TYPE_CONFIG 100
@@ -264,6 +264,7 @@ struct payloadPedalConfig
   uint8_t kf_Joystick_u8;
   uint8_t kf_modelNoise_joystick;
   uint8_t servoIdleTimeout;
+  uint8_t positionSmoothingFactor_u8;
 };
 
 struct payloadESPNowInfo
