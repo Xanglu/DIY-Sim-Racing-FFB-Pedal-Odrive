d0772d00f22684412c772adcc4da213c2b456e64
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2025-09-30 04:07:17 +0800

    replace the OTA binary for esp32 related board

--- OTA/copy_to_OTA_folder_ControlBoard.bat
+++ OTA/copy_to_OTA_folder_ControlBoard.bat
@@ -1,21 +1,4 @@
 @echo off
-set source=..\ESP32\.pio\build\esp32\firmware.bin
-set destination=..\OTA\ControlBoard\esp32\
-
-echo Copying %source% to %destination%...
-xcopy "%source%" "%destination%" /y
-
-set source=..\ESP32\.pio\build\esp32\bootloader.bin
-set destination=..\OTA\ControlBoard\esp32\
-
-echo Copying %source% to %destination%...
-xcopy "%source%" "%destination%" /y
-
-set source=..\ESP32\.pio\build\esp32\partitions.bin
-set destination=..\OTA\ControlBoard\esp32\
-
-echo Copying %source% to %destination%...
-xcopy "%source%" "%destination%" /y
 
 set source=..\ESP32\.pio\build\esp32s3usbotg\firmware.bin
 set destination=..\OTA\ControlBoard\esp32S3\
@@ -67,24 +50,6 @@ xcopy "%source%" "%destination%" /y
 
 set source=..\ESP32\.pio\build\esp32s3usbotg-gilphilbert_2_0\partitions.bin
 set destination=..\OTA\ControlBoard\Gilphilbert_2_0\
-
-echo Copying %source% to %destination%...
-xcopy "%source%" "%destination%" /y
-
-set source=..\ESP32\.pio\build\esp32-speedcrafter\firmware.bin
-set destination=..\OTA\ControlBoard\Speedcrafter\
-
-echo Copying %source% to %destination%...
-xcopy "%source%" "%destination%" /y
-
-set source=..\ESP32\.pio\build\esp32-speedcrafter\bootloader.bin
-set destination=..\OTA\ControlBoard\Speedcrafter\
-
-echo Copying %source% to %destination%...
-xcopy "%source%" "%destination%" /y
-
-set source=..\ESP32\.pio\build\esp32-speedcrafter\partitions.bin
-set destination=..\OTA\ControlBoard\Speedcrafter\
 
 echo Copying %source% to %destination%...
 xcopy "%source%" "%destination%" /y
